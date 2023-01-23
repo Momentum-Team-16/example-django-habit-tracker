96ce9bcd583fc043df8a034dd2fb6612c7c97a8f
Amy Gori <amydgori@gmail.com>
2023-01-23 13:29:37 -0500

    Switch settings to use Postgres

--- config/settings.py
+++ config/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dj-ht',
+        'USER': 'dj-ht',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
